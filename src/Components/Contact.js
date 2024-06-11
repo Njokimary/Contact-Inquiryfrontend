968240a5ed344755a6e5dd2b27c2169fefce88bf
Njokimary <marynjoki738@gmail.com>
2024-06-11 16:58:36 +0300

    incorporate the logo with the form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+
 const Contact = () => {
     const [formData, setFormData] = useState({ name: '', email: '', subject: '', message: '' });
     const [errors, setErrors] = useState({});
@@ -22,7 +23,7 @@ const Contact = () => {
             });
             setSuccessMessage(response.data.message);
             setErrors({});
-            setFormData({ name: '', email: '', subject: '', message: '' }); // Clear form fields after successful submission
+            setFormData({ name: '', email: '', message: '' }); // Clear form fields after successful submission
         } catch (error) {
             if (error.response && error.response.status === 422) {
                 setErrors(error.response.data);
@@ -33,6 +34,9 @@ const Contact = () => {
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                <div className="flex justify-center mb-8">
+                    <img src="/Logo-Dawit.png" alt="Logo" className="h-16 w-auto" />
+                </div>
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
                 <form onSubmit={handleSubmit} className="space-y-8">
@@ -74,7 +78,7 @@ const Contact = () => {
                         />
                         {errors.message && <div className="text-red-500">{errors.message}</div>}
                     </div>
-                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
+                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-[#9eb316] sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
                     {successMessage && <div className="text-green-500">{successMessage}</div>}
                 </form>
             </div>
